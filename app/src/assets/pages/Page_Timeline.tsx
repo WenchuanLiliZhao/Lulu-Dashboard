ce616a3a26bd81a86c5bfaeccef4c189c3a823de
Wenchuan Zhao <111623150+WenchuanLiliZhao@users.noreply.github.com>
2025-06-03 11:24:46 +0800

    u

--- app/src/assets/pages/Page_Timeline.tsx
+++ app/src/assets/pages/Page_Timeline.tsx
@@ -1,6 +1,16 @@
-import { groupIssuesByField, Example_Issues } from "../Components/Timeline/Shapes";
+import { Example_Issues_1 } from "../Components/Timeline/ExampleData/Example_Issues_1";
+import { Example_Issues_2 } from "../Components/Timeline/ExampleData/Example_Issues_2";
+import { Example_Issues_3 } from "../Components/Timeline/ExampleData/Example_Issues_3";
+import { groupIssuesByField } from "../Components/Timeline/Shapes";
 import { Timeline } from "../Components/Timeline/Timeline";
 import type { PageShape } from "../object-shapes/Page";
+
+
+const Example_Issues = [
+  ...Example_Issues_1,
+  ...Example_Issues_2,
+  ...Example_Issues_3,
+]
 
 const Page_Timeline: PageShape = {
   info: {
